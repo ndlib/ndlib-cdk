026f709f4dfc4e61b939e15abbf70b57aa620289
Justin Gondron <jgondron@nd.edu>
2019-09-23 14:03:15 -0400

    Patch version
    Need to bump the version since I previously published 1.0.0 to npm as a test. Also ran prettier to fix some issues

--- src/stack-tags.ts
+++ src/stack-tags.ts
@@ -6,12 +6,12 @@ import { ConstructNode, IAspect, IConstruct, Stack } from "@aws-cdk/core";
 export class StackTags implements IAspect {
   public visit(node: IConstruct) {
     if (node instanceof Stack) {
-      const projectName = this.getContext('projectName', node.node);
+      const projectName = this.getContext("projectName", node.node);
       // Not sure this makes sense to do for multistack applications, but for now it applies only
       // one description to all stacks. Perhaps this should just change to test if this tag was added
-      const description = this.getContext('description', node.node);
-      const contact = this.getContext('contact', node.node);
-      const owner = this.getContext('owner', node.node);
+      const description = this.getContext("description", node.node);
+      const contact = this.getContext("contact", node.node);
+      const owner = this.getContext("owner", node.node);
 
       node.tags.setTag("ProjectName", projectName);
       node.tags.setTag("Name", node.stackName);
@@ -22,11 +22,11 @@ export class StackTags implements IAspect {
   }
 
   /**
-   * 
+   *
    * @param keyName The name of the key to get from context
    * @param node The node to add errors to if the key does not exist
    */
-  private getContext(keyName: string, node: ConstructNode) : string {
+  private getContext(keyName: string, node: ConstructNode): string {
     const value = node.tryGetContext(keyName);
     if (value === undefined) {
       node.addError(`Expected context key "${keyName}"`);

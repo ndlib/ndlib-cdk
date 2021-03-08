6c9a0dd6fc7fdcd3b0c5b677be28011b3dd49632
Ian Alford <ialford@nd.edu>
2021-03-08 14:24:40 -0500

    feat(docker-codebuild): factory helper for using authentication and DockerHub images in CodeBuild (#53)
    * docs: update documentation to include new scope and factory method examples
    
    * chore: update dependencies
    
    * feat(docker-codebuild): factory helper for using authentication and DockerHub images in CodeBuild
    
    This method provides a simpler way to create a CodeBuild project that uses authenticated pulls from DockerHub. It does not affect the base CDK constructs, but rather allows the CodeBuilds to be made by only passing in two strings to the method.
    
    Closes #43

---
+++ src/docker-codebuild-action.ts
@@ -0,0 +1,40 @@
+import { DockerImageOptions, IBuildImage, LinuxBuildImage, WindowsBuildImage } from '@aws-cdk/aws-codebuild';
+import { Secret } from '@aws-cdk/aws-secretsmanager';
+import cdk = require('@aws-cdk/core');
+
+export interface IDockerCodeBuildAction extends DockerImageOptions {
+  /**
+   * Defines the image and tag of a container image on DockerHub to be used in a CodeBuild Project
+   *
+   * Can be defined as `image` or `image:tag`
+   */
+  readonly image: string;
+  /**
+   * Defines the path in Secrets Manager where DockerHub Credentials are stored.
+   *
+   * @see  https://aws.amazon.com/premiumsupport/knowledge-center/codebuild-docker-pull-image-error/
+   */
+  readonly credentialsContextKeyName: string;
+}
+export class DockerCodeBuildAction {
+  public static fromLinuxDockerImage(scope: cdk.Construct, id: string, options: IDockerCodeBuildAction): IBuildImage {
+    return LinuxBuildImage.fromDockerRegistry(options.image, {
+      secretsManagerCredentials: Secret.fromSecretNameV2(scope, `${id}-Credentials`, options.credentialsContextKeyName),
+    });
+  }
+
+  /**
+   * Returns a Windows Build Image using the provided DockerHub Image tag and auth credentials
+   *
+   * @param scope The scope that this image is being created within
+   * @param id A unique identifier for this image
+   * @param credentialsContextKeyName A required path to look for the credentials secret in Secrets Manager.
+   * @param image The DockerHub image that should be used for this action
+   */
+
+  public static fromWindowsDockerImage(scope: cdk.Construct, id: string, options: IDockerCodeBuildAction): IBuildImage {
+    return WindowsBuildImage.fromDockerRegistry(options.image, {
+      secretsManagerCredentials: Secret.fromSecretNameV2(scope, `${id}-Credentials`, options.credentialsContextKeyName),
+    });
+  }
+}

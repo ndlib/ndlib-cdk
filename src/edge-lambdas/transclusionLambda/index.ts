d61dcd395b83df2487116e6c078068eebbee0915
Michael Kirkpatrick <mkirkpatrick@nd.edu>
2021-04-26 15:20:36 -0400

    Add bucket read permission to TransclusionLambda. Refactor unit tests.

--- src/edge-lambdas/transclusionLambda/index.ts
+++ src/edge-lambdas/transclusionLambda/index.ts
@@ -1,15 +1,24 @@
 /* tslint:disable:max-classes-per-file */
 import { Behavior, CloudFrontAllowedMethods, LambdaEdgeEventType } from '@aws-cdk/aws-cloudfront';
+import { PolicyStatement } from '@aws-cdk/aws-iam';
 import * as lambda from '@aws-cdk/aws-lambda';
+import { Bucket } from '@aws-cdk/aws-s3';
 import * as cdk from '@aws-cdk/core';
 import * as path from 'path';
 import { IEdgeLambda, IEdgeLambdaProps } from '../index';
+
+export interface ITransclusionLambdaProps extends IEdgeLambdaProps {
+  /**
+   * The bucket where site files are hosted. Needed to grant permissions so the lambda can fetch objects.
+   */
+  originBucket: Bucket;
+}
 
 export class TransclusionLambda extends cdk.Construct implements IEdgeLambda {
   public readonly function: lambda.Function;
   public readonly behavior: Behavior;
 
-  constructor(scope: cdk.Construct, id: string, props: IEdgeLambdaProps) {
+  constructor(scope: cdk.Construct, id: string, props: ITransclusionLambdaProps) {
     super(scope, id);
 
     this.function = new lambda.Function(scope, `${id}Function`, {
@@ -20,6 +29,12 @@ export class TransclusionLambda extends cdk.Construct implements IEdgeLambda {
       timeout: cdk.Duration.seconds(10),
       ...props,
     });
+    this.function.addToRolePolicy(
+      new PolicyStatement({
+        resources: [props.originBucket.bucketArn + '/*'],
+        actions: ['s3:GetObject*'],
+      }),
+    );
 
     this.behavior = {
       allowedMethods: CloudFrontAllowedMethods.GET_HEAD_OPTIONS,
--- tests/edge-lambdas/spaRedirectionLambda/index.test.ts
+++ tests/edge-lambdas/spaRedirectionLambda/index.test.ts
@@ -4,38 +4,44 @@ import { Stack, Duration } from '@aws-cdk/core';
 import { SpaRedirectionLambda } from '../../../src/edge-lambdas/spaRedirectionLambda';
 
 describe('SpaRedirectionLambda', () => {
-  test('creates lambda function with default props', () => {
+  describe('with default props', () => {
     const stack = new Stack();
     const testLambda = new SpaRedirectionLambda(stack, 'Lambda', {
       isDefaultBehavior: false,
     });
-    expect(testLambda.behavior).toMatchObject({
-      allowedMethods: CloudFrontAllowedMethods.GET_HEAD_OPTIONS,
-      compress: true,
-      isDefaultBehavior: false,
-      lambdaFunctionAssociations: [
-        {
-          eventType: LambdaEdgeEventType.ORIGIN_REQUEST,
-          lambdaFunction: testLambda.function.currentVersion,
-        },
-      ],
-    });
-    expectCDK(stack).to(
-      haveResourceLike('AWS::Lambda::Function', {
-        Description: 'Basic rewrite rule to send directory requests to appropriate locations in the SPA.',
-        Handler: 'handler.handler',
-        Runtime: 'nodejs12.x',
-        Timeout: 10,
-        Environment: {
-          Variables: {
-            EXTENSIONS: '.html,.js,.json,.css,.jpg,.jpeg,.png,.ico,.map,.txt,.kml,.svg,.webmanifest,.webp,.xml,.zip',
+
+    test('creates function', () => {
+      expectCDK(stack).to(
+        haveResourceLike('AWS::Lambda::Function', {
+          Description: 'Basic rewrite rule to send directory requests to appropriate locations in the SPA.',
+          Handler: 'handler.handler',
+          Runtime: 'nodejs12.x',
+          Timeout: 10,
+          Environment: {
+            Variables: {
+              EXTENSIONS: '.html,.js,.json,.css,.jpg,.jpeg,.png,.ico,.map,.txt,.kml,.svg,.webmanifest,.webp,.xml,.zip',
+            },
           },
-        },
-      }),
-    );
+        }),
+      );
+    });
+
+    test('creates valid behavior configuration', () => {
+      expect(testLambda.behavior).toMatchObject({
+        allowedMethods: CloudFrontAllowedMethods.GET_HEAD_OPTIONS,
+        compress: true,
+        isDefaultBehavior: false,
+        lambdaFunctionAssociations: [
+          {
+            eventType: LambdaEdgeEventType.ORIGIN_REQUEST,
+            lambdaFunction: testLambda.function.currentVersion,
+          },
+        ],
+      });
+    });
   });
 
-  test('creates lambda function with overridden props', () => {
+  describe('with overridden props', () => {
     const stack = new Stack();
     const testLambda = new SpaRedirectionLambda(stack, 'Lambda', {
       description: 'My transclusion lambda for my fancy stack.',
@@ -47,33 +53,39 @@ describe('SpaRedirectionLambda', () => {
       defaultTtl: Duration.seconds(150),
       fileExtensions: ['.pdf', '.abcde'],
     });
-    expect(testLambda.behavior).toMatchObject({
-      allowedMethods: CloudFrontAllowedMethods.GET_HEAD_OPTIONS,
-      compress: true,
-      isDefaultBehavior: true,
-      pathPattern: '/public/',
-      lambdaFunctionAssociations: [
-        {
-          eventType: LambdaEdgeEventType.ORIGIN_REQUEST,
-          lambdaFunction: testLambda.function.currentVersion,
-        },
-      ],
-      minTtl: Duration.seconds(100),
-      maxTtl: Duration.seconds(200),
-      defaultTtl: Duration.seconds(150),
-    });
-    expectCDK(stack).to(
-      haveResourceLike('AWS::Lambda::Function', {
-        Description: 'My transclusion lambda for my fancy stack.',
-        Handler: 'handler.handler',
-        Runtime: 'nodejs12.x',
-        Timeout: 123,
-        Environment: {
-          Variables: {
-            EXTENSIONS: '.pdf,.abcde',
+
+    test('overrides defaults on function', () => {
+      expectCDK(stack).to(
+        haveResourceLike('AWS::Lambda::Function', {
+          Description: 'My transclusion lambda for my fancy stack.',
+          Handler: 'handler.handler',
+          Runtime: 'nodejs12.x',
+          Timeout: 123,
+          Environment: {
+            Variables: {
+              EXTENSIONS: '.pdf,.abcde',
+            },
           },
-        },
-      }),
-    );
+        }),
+      );
+    });
+
+    test('overrides defaults on behavior', () => {
+      expect(testLambda.behavior).toMatchObject({
+        allowedMethods: CloudFrontAllowedMethods.GET_HEAD_OPTIONS,
+        compress: true,
+        isDefaultBehavior: true,
+        pathPattern: '/public/',
+        lambdaFunctionAssociations: [
+          {
+            eventType: LambdaEdgeEventType.ORIGIN_REQUEST,
+            lambdaFunction: testLambda.function.currentVersion,
+          },
+        ],
+        minTtl: Duration.seconds(100),
+        maxTtl: Duration.seconds(200),
+        defaultTtl: Duration.seconds(150),
+      });
+    });
   });
 });
